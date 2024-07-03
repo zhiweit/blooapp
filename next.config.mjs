63ccdee1971ed1527889df7caeb7a63ff6cb018f
mightymagnus <theanzhiwei@gmail.com>
2024-07-03 22:51:59 +0800

    chore: configure backend prod url in next config

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,8 @@ const nextConfig = {
   async rewrites() {
     return [{
       source: "/api/:path*",
-      // destination: "https://blooapp-nakavwocwa-et.a.run.app/api/:path*",
-      destination: "http://localhost:8080/api/:path*",
+      destination: "https://blooapp-nakavwocwa-et.a.run.app/api/:path*", // prod
+      // destination: "http://localhost:8080/api/:path*", // local dev
     }, ];
   },
 };
